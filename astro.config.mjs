08b8d272126343310cac544393b738c922ce9b62
Max <max@maxtana.com>
2025-04-02 18:29:59 -0500

    added date & desc to post links, updated socaisl in footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import preact from "@astrojs/preact";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://max-phillips-dot-com.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact(), icon()]
 });
---
+++ src/data/socials.js
@@ -0,0 +1,23 @@
+
+  export const socials = [
+    {
+      name: "Bluesky",
+      url: "https://bsky.app/profile/maxtana.bsky.social",
+      icon: "bluesky"
+    },
+    {
+      name: "YouTube",
+      url: "https://www.youtube.com/@Maxtana",
+      icon: "youtube"
+    },
+    {
+      name: "TikTok",
+      url: "https://www.tiktok.com/@maxtana",
+      icon: "tiktok"
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/maxtana",
+      icon: "github"
+    }
+  ];
