e635b55c45b0925989c227c4ec0667ff11acf88a
Max <max@maxtana.com>
2025-04-03 18:54:15 -0500

    Added PartyTown and Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 import icon from "astro-icon";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://max-phillips-dot-com.netlify.app/",
-  integrations: [preact(), icon()]
+  integrations: [preact(), icon(), partytown()]
 });
